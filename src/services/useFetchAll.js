7a720ee0b99a48ebd109e6e0c010dab0fd9d0ad7
mohamed-nawas <mgmnawas@gmail.com>
2025-05-18 12:05:58 +0530

    optimized for gh-pages deployment-fix7

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,34 +1,34 @@
-import React from "react";
+// import React from "react";
 
-export default function useFetchAll(urls) {
-  const isMounted = React.useRef(false);
-  const [data, setData] = React.useState(null);
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState(null)
+// export default function useFetchAll(urls) {
+//   const isMounted = React.useRef(false);
+//   const [data, setData] = React.useState(null);
+//   const [loading, setLoading] = React.useState(true);
+//   const [error, setError] = React.useState(null)
 
-  React.useEffect(() => {
-    isMounted.current = true;
-    const promises = urls.map((url) =>
-      fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
-        if (response.ok) return response.json();
-        throw response;
-      })
-    );
+//   React.useEffect(() => {
+//     isMounted.current = true;
+//     const promises = urls.map((url) =>
+//       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
+//         if (response.ok) return response.json();
+//         throw response;
+//       })
+//     );
 
-    Promise.all(promises)
-      .then((json) => {
-        if (isMounted.current) setData(json);
-      })
-      .catch((e) => {
-        console.error(e);
-        if (isMounted.current) setError(e);
-      })
-      .finally(() => {
-        if (isMounted.current) setLoading(false);
-      });
+//     Promise.all(promises)
+//       .then((json) => {
+//         if (isMounted.current) setData(json);
+//       })
+//       .catch((e) => {
+//         console.error(e);
+//         if (isMounted.current) setError(e);
+//       })
+//       .finally(() => {
+//         if (isMounted.current) setLoading(false);
+//       });
 
-    return () => (isMounted.current = false);
-  }, [urls]);
+//     return () => (isMounted.current = false);
+//   }, [urls]);
 
-  return { data, loading, error };
-}
+//   return { data, loading, error };
+// }
