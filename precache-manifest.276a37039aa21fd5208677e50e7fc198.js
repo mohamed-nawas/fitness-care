cfee2aac05fcc829377b2c2cdbadd8c952502317
mohamed-nawas <mohamed-nawas@users.noreply.github.com>
2025-05-18 06:23:58 +0000

    deploy: cba89bde9a7790a8795ad0fa601e10bbc98005fe

---
+++ precache-manifest.276a37039aa21fd5208677e50e7fc198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67dbfbb1e36010617101aa874f51b980",
+    "url": "/fitness-care/index.html"
+  },
+  {
+    "revision": "ef3a885edf553404a06e",
+    "url": "/fitness-care/static/css/main.e42d54fa.chunk.css"
+  },
+  {
+    "revision": "a3f96c187718cdb67634",
+    "url": "/fitness-care/static/js/2.16329ca9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fitness-care/static/js/2.16329ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3a885edf553404a06e",
+    "url": "/fitness-care/static/js/main.6028076b.chunk.js"
+  },
+  {
+    "revision": "bf6b6a44678264c7dbdd",
+    "url": "/fitness-care/static/js/runtime-main.b3fff437.js"
+  }
+]);
